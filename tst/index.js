23d4954fa13a2b21c33ef9d17568957c44d0940b
Deepak Nayak <deepak201@gmail.com>
2022-02-16 19:18:07 -0800

    Fix regex to account for dot in ddb table name

--- tst/index.js
+++ tst/index.js
@@ -6,6 +6,7 @@ chai.use(require('sinon-chai'));
 const path = require('path');
 const fs = require('fs');
 const AWS = require('aws-sdk-mock');
+const dynamodb = require('../src/dynamodb');
 AWS.setSDK(path.resolve('node_modules/aws-sdk'));
 
 const EVENTS = {
@@ -229,5 +230,35 @@ describe('index.handler', () => {
                 expect(err.message).to.equal('FAILURE');
             }
         });
+    });
+});
+
+describe('dynamodb.getTableNameFromArn', () => {
+    describe('table name has alphanumeric only', () => {
+        it('simple table name', () => {
+            const tableName = "arn:aws:dynamodb:us-west-2:111122223333:table/TestTable123/stream/2015–05–11T21:21:33.291";
+            expect(dynamodb.getTableNameFromArn(tableName)).to.equal('TestTable123');
+        })
+    });
+
+    describe('table name has dash', () => {
+        it('table name with dash', () => {
+            const tableName = "arn:aws:dynamodb:us-west-2:111122223333:table/TestTable-123/stream/2015–05–11T21:21:33.291";
+            expect(dynamodb.getTableNameFromArn(tableName)).to.equal('TestTable-123');
+        })
+    });
+
+    describe('table name has underscore', () => {
+        it('table name with underscore', () => {
+            const tableName = "arn:aws:dynamodb:us-west-2:111122223333:table/TestTable_123/stream/2015–05–11T21:21:33.291";
+            expect(dynamodb.getTableNameFromArn(tableName)).to.equal('TestTable_123');
+        })
+    });
+
+    describe('table name has dot', () => {
+        it('table name with dot', () => {
+            const tableName = "arn:aws:dynamodb:us-west-2:111122223333:table/TestTable.123/stream/2015–05–11T21:21:33.291";
+            expect(dynamodb.getTableNameFromArn(tableName)).to.equal('TestTable.123');
+        })
     });
 });
